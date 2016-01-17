0dfbb45543b84638d0006ed5bfa8a7668c0ef55a
Jimmy Pocock <jimmypocock@yahoo.com>
2016-01-17 15:01:23 -0600

    Created Enemeny Formation and Spawner script.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	void Start () {
+		// Instantiate an enemy object on Start
+		GameObject enemy = Instantiate ( enemyPrefab, new Vector3 ( 0, 0, 0 ), Quaternion.identity ) as GameObject;
+
+		// Set enemy spawn to location of EnemyFormation
+		enemy.transform.parent = transform;
+	}
+
+	void Update () {
+		
+	}
+}
