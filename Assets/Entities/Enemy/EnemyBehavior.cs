44b2b56c976ab9bcee8c6981450dd5b2627d0209
Jimmy Pocock <jimmypocock@yahoo.com>
2016-01-17 18:16:09 -0600

    Projectile destroying Enemy

---
+++ Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+	public float health = 150;
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Projectile projectile = collider.gameObject.GetComponent<Projectile> ();
+
+		if (projectile) {
+			health -= projectile.GetDamage ();
+			projectile.Hit ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
